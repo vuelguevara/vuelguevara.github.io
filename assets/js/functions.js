0dccc18ca25a8141c194cf8aa5c9a9d23f437fb7
vuelguevara <vuelguevara@gmail.com>
2017-12-24 20:07:48 +0800

    added work section interactions

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,29 @@
-$( document ).ready(function() {
+function smoothScroll (duration) {
+  $('a[href^="#"]').on('click', function(event) {
 
-  //
+    var target = $( $(this).attr('href') );
 
-});
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: target.offset().top
+      }, duration);
+    }
+  });
+}
+
+function workBelt() {
+
+  $('.thumb-unit').click(function() {
+    $('.work-belt').css('left', '-100%');
+  });
+
+  $('.work-return').click(function(){
+    $('.work-belt').css('left', '0%');
+  });
+}
+
+$(function(){
+  smoothScroll(400);
+  workBelt();
+})
