bb00be6a008e43508b7046780b3869d4287c48e4
vuelguevara <vuelguevara@gmail.com>
2018-01-07 22:33:10 +0800

    added slide animation

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -42,8 +42,48 @@ function workLoad() {
 
 }
 
+function clientSlide() {
+  $('.client-logo').click(function() {
+    var $this = $(this),
+        $siblings = $this.parent().children(),
+        position = $siblings.index($this);
+        
+    $('.client-slide').removeClass('client-slide--active animated flipInY').eq(position).addClass('client-slide--active animated flipInY');
+    $siblings.removeClass('client-logo--active');
+    $this.addClass('client-logo--active');        
+  });
+
+  $('.next-button, .back-button').click(function() {
+    var $this = $(this),
+        curActiveClient = $('.client-slides-container').find('.client-slide--active', '.client-logo--active'),
+        position = $('.client-slides-container').children().index(curActiveClient),
+        clientNum = $('.client-slide').length;
+
+      if($this.hasClass('next-button')) {
+        if(position < clientNum -1){
+          $('.client-slide--active').removeClass('client-slide--active animated flipInY').next().addClass('client-slide--active animated flipInY');
+          $('.client-logo--active').removeClass('client-logo--active').next().addClass('client-logo--active');
+      } else {
+          $('.client-slide').removeClass('client-slide--active animated flipInY').first().addClass('client-slide--active animated flipInY');
+          $('.client-logo').removeClass('client-logo--active').first().addClass('client-logo--active');
+      }
+    } else {
+
+      if(position === 0){
+        $('.client-slide').removeClass('client-slide--active animated flipInY').last().addClass('client-slide--active animated flipInY');
+        $('.client-logo').removeClass('client-logo--active').last().addClass('client-logo--active');
+    } else {
+        $('.client-slide--active').removeClass('client-slide--active animated flipInY').prev().addClass('client-slide--active animated flipInY');
+        $('.client-logo--active').removeClass('client-logo--active').prev().addClass('client-logo--active');
+    }
+    }
+  });
+
+}
+
 $(function(){
   smoothScroll(400);
   workBelt();
   workLoad();
+  clientSlide();
 })
